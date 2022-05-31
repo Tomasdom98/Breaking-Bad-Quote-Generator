e68f1cba3dc31af9277fa9c98d16ee2287f124b7
Livia Ellen <liviaellen@msn.com>
2022-05-31 09:34:04 +0800

    push

--- bbquote883/app.py
+++ bbquote883/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from bbquote839.lib import get_quote
+from bbquote883.lib import get_quote
 
 author, quote = get_quote()  # assuming the function returns an author and a quote
 
