2061106c87432143c41ff97c27c337a7a898cd27
Livia Ellen <liviaellen@msn.com>
2022-05-31 09:38:45 +0800

    push

--- bbquote883/app.py
+++ bbquote883/app.py
@@ -4,5 +4,5 @@ from bbquote883.lib import get_quote
 
 author, quote = get_quote()  # assuming the function returns an author and a quote
 
-st.title('Welcome to my BB quote generator ! Now working')
+st.title('Welcome to my Breaking Bad quote generator ! Made with ❤️ - Bali 883 Le Wagon')
 f"Quote : {quote}, \n> {author}"
